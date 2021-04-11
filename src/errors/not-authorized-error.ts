567cecaaa2a978f4b17e74a6757f117931a8e015
pratikg17 <pjgawand@gmail.com>
2021-04-11 21:36:55 +0530

    Updates

--- src/errors/not-authorized-error.ts
+++ src/errors/not-authorized-error.ts
@@ -1,11 +1,14 @@
-import { CustomError } from '../errors/custom-error';
+import { CustomError } from './custom-error';
 
 export class NotAuthorizedError extends CustomError {
   statusCode = 401;
+
   constructor() {
-    super('Not Authorized!');
+    super('Not Authorized');
+
     Object.setPrototypeOf(this, NotAuthorizedError.prototype);
   }
+
   serializeErrors() {
     return [{ message: 'Not authorized' }];
   }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
+
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
-export * from './middlewares/not-authorized-error';
-export * from './middlewares/middleware';
+export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
