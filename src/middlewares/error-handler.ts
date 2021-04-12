32364eb8b2b43922569b82af10c54d2202cf8cd6
pratikg17 <pjgawand@gmail.com>
2021-04-12 13:45:54 +0530

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -10,8 +10,8 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
-
+  console.error(err);
   res.status(400).send({
-    errors: [{ message: 'Something went wrong' }]
+    errors: [{ message: 'Something went wrong' }],
   });
 };
