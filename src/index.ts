57e695fecac1ea8949ed675c5e402b9d83bc4af9
pratikg17 <pjgawand@gmail.com>
2021-04-16 16:55:13 +0530

    Add NATS server changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Reexport middleware and erros
+// Re-export stuff from errors and middlewares
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
@@ -10,3 +10,9 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/subjects';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
